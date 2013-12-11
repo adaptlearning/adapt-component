b6ab95637ca6ba6934e35d9a82c85541e4e7394b
Daryl Hedley <darylhedley@hotmail.com>
2013-12-11 13:58:58 +0000

    Updated view comments

--- js/adapt-contrib-componentName.js
+++ js/adapt-contrib-componentName.js
@@ -4,7 +4,17 @@ define(["coreViews/componentView", "coreJS/adapt"], function(ComponentView, Adap
         
         postRender: function() {
             console.log("rendering");
+
+            // IMPORTANT! 
+            // Both of the following methods need to be called inside your view.
+
+            // Use this to set the model status to ready. 
+            // It should be used when telling Adapt that this view is completely loaded.
+            // This is sometimes used in conjunction with imageReady.
             this.setReadyStatus();
+
+            // Use this to set the model status to complete.
+            // This can be used with inview or when the model is set to complete/the question has been answered.
             this.setCompletionStatus();
         }
         
