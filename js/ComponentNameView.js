bf9b26f2738419b8e36d7d09e07d3d8d939ec6f7
Eleanor Heath <eleanor.heath@learningpoool.com>
2022-05-10 11:15:37 +0100

    2808 - addressed PR comments; moved view into its own file

---
+++ js/ComponentNameView.js
@@ -0,0 +1,30 @@
+import Adapt from 'core/js/adapt';
+import ComponentView from 'core/js/views/componentView';
+
+class ComponentNameView extends ComponentView {
+
+  preRender() {
+    console.log('preRender');
+    // here is a good place to add any code you want to run prior to the component being rendered
+  }
+
+  postRender() {
+    console.log('postRender');
+    // IMPORTANT!
+    // Both of the following methods need to be called inside your view.
+
+    // Use this to set the model status to ready.
+    // It should be used when telling Adapt that this view is completely loaded.
+    // This is sometimes used in conjunction with imageReady.
+    this.setReadyStatus();
+
+    // Use this to set the model status to complete.
+    // This can be used with inview or when the model is set to complete/the question has been answered.
+    this.setCompletionStatus();
+  }
+
+}
+
+ComponentNameView.template = 'componentName';
+
+export default ComponentNameView;
--- js/adapt-contrib-componentName.js
+++ js/adapt-contrib-componentName.js
@@ -1,32 +1,8 @@
 import Adapt from 'core/js/adapt';
-import ComponentView from 'core/js/views/componentView';
+import ComponentNameModel from './ComponentNameModel';
+import ComponentNameView from './ComponentNameView';
 
-class ComponentName extends ComponentView {
-
-  get template() {
-    return 'componentName';
-  }
-
-  preRender() {
-    console.log('preRender');
-    // here is a good place to add any code you want to run prior to the component being rendered
-  }
-
-  postRender() {
-    console.log('postRender');
-    // IMPORTANT!
-    // Both of the following methods need to be called inside your view.
-
-    // Use this to set the model status to ready.
-    // It should be used when telling Adapt that this view is completely loaded.
-    // This is sometimes used in conjunction with imageReady.
-    this.setReadyStatus();
-
-    // Use this to set the model status to complete.
-    // This can be used with inview or when the model is set to complete/the question has been answered.
-    this.setCompletionStatus();
-  }
-
-}
-
-export default Adapt.register('componentName', ComponentName);
+export default Adapt.register('componentName', {
+  model: ComponentNameModel,
+  view: ComponentNameView
+});
